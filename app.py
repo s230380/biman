f6456b4d88d79dcc9256d3dd349813c172f6f930
s230380 <gimpo.s230380@ggh.goe.go.kr>
2024-11-19 15:06:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,16 +14,16 @@ with col1:
       st.write(' - 기계학습 알고리즘 : 로지스틱 회귀 ')
       st.write(' - 학습 데이터 출처 : https://www.kaggle.com/datasets/muhramasaputra/obesity-based-on-eating-habits-and-physical-cond')
       st.write(' - 훈련    데이터 : 2111건')
-      st.write(' - 테스트 데이터 : *건')
-      st.write(' - 모델 정확도 : 0.89')
+      st.write(' - 테스트 데이터 : 634건')
+      st.write(' - 모델 정확도 : 0.74')
 
 # 3. 데이터시각화
 with col2:
       st.subheader('데이터시각화1')
-      st.image('____________' )   # 이미지 불러오기
+      st.image('시각화1.png' )   # 이미지 불러오기
 with col3:
       st.subheader('데이터시각화2')
-      st.image('____________') 
+      st.image('시각화2.png') 
 
 # 4. 모델 활용
 st.subheader('모델 활용')
@@ -33,8 +33,8 @@ a = st.number_input(' 나이를 입럭해 ', value=0)   # 사용자 입력
 
 if st.button('합불분류'):              # 사용자가 '합불분류' 버튼을 누르면
         input_data = [[ a ]]          # 사용자가 입력한 a,b,c 를 input_data에 저장하고
-        p = model._______(input_data)      # model이 분류한 값을 p에 저장한다
+        p = model.predict(input_data)      # model이 분류한 값을 p에 저장한다
         if p[0] == 1 :
-              st.success('인공지능 분류 결과는 ___입니다')
+              st.success('비만으로 예측됩니다.')
         else:
-              st.success('인공지능 분류 결과를 ____입니다')
+              st.success('비만이 아닌 것으로 예측됩니다.')
